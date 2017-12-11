dc8c9fc93ab991f90c5ccb3445eee43ec00357f5
Shahin Fotowat <sfotowat1@saddleback.edu>
2017-12-11 13:53:01 -0800

    added the description

--- tour.h
+++ tour.h
@@ -16,30 +16,73 @@ class Tour : public QDialog
     Q_OBJECT
 
 public:
+    /**
+     * @brief Tour
+     * @param parent
+     */
     explicit Tour(QWidget *parent = 0);
     ~Tour();
 
 private slots:
+    /**
+     * @brief on_pushButton_back_clicked
+     */
     void on_pushButton_back_clicked();
+
+    /**
+     * @brief on_pushButton_add_clicked
+     */
     void on_pushButton_add_clicked();
+
+    /**
+     * @brief on_pushButton_delete_clicked
+     */
     void on_pushButton_delete_clicked();
+
+    /**
+     * @brief displayselectedStadiums
+     */
     void displayselectedStadiums();
 
+    /**
+     * @brief on_button_laTrip_clicked
+     */
     void on_button_laTrip_clicked();
 
+    /**
+     * @brief on_pushButton_clicked
+     */
     void on_pushButton_clicked();
 
+    /**
+     * @brief on_button_laStartTour_clicked
+     */
     void on_button_laStartTour_clicked();
 
+    /**
+     * @brief on_button_customOrder_clicked
+     */
     void on_button_customOrder_clicked();
 
+    /**
+     * @brief on_button_startTour_clicked
+     */
     void on_button_startTour_clicked();
 
+    /**
+     * @brief on_button_fordTrip_clicked
+     */
     void on_button_fordTrip_clicked();
 
 private:
+    /**
+     * @brief ui
+     */
     Ui::Tour *ui;
 
+    /**
+     * @brief selectedStadiums
+     */
     std::vector<QString> selectedStadiums;
 };
 

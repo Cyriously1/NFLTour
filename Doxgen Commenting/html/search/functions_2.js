626ddd9aecd590ec0d062252af043d962a75f836
Shahin Fotowat <sfotowat1@saddleback.edu>
2017-12-13 16:33:49 -0800

    updated the doxygen

--- Doxgen Commenting/html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['createeditor',['createEditor',['../class_delegate.html#a3abc4fe9f16fa7c5e64ffcf48cced054',1,'Delegate']]]
-];
--- Doxgen Commenting/html/search/functions_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['isadmin',['isAdmin',['../class_admin_login.html#a7b096251d552fafeae34a229e748398c',1,'AdminLogin']]]
-];
